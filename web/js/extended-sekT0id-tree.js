0bf903c2336bf0b636e1fff243c13c480069d84c
sekT0id <pop_korn@mail.ru>
2016-09-28 16:33:08 +0300

    js tree fix

--- web/js/extended-sekT0id-tree.js
+++ web/js/extended-sekT0id-tree.js
@@ -1,10 +1,7 @@
 $(document).ready(function () {
     function init() {
         $('.easy-tree').EasyTree({
-            addable: false,
-            editable: false,
-            deletable: false,
-            selectable: false
+            expanded: false
         });
     }
 
