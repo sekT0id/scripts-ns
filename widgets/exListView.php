348335919a9a34bfc0a05149dc8a2debc5113bfc
Alex Prusov <Alex Prusov>
2016-10-03 22:58:31 +0300

    cls

--- widgets/exListView.php
+++ widgets/exListView.php
@@ -11,32 +11,14 @@ use app\models\SessionsSearch;
 
 class exListView extends BaseWidget
 {
-    public $pageSize = 10;
-
-    protected function getData()
-    {
-        $this->model = new ActiveDataProvider([
-            'query' => Sessions::find()
-                ->with('details.script')
-                ->orderBy('id DESC'),
-            'pagination' => [
-                'pageSize' => $this->pageSize,
-            ],
-        ]);
-    }
-
     public function init()
     {
         parent::init();
-
-        //$this->getData();
-        //$this->data['searchModel'] = $this->model;
 
         $searchModel = new SessionsSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
         $this->data['model'] = $searchModel;
-        //$this->data['searchModel'] = $searchModel;
         $this->data['dataProvider'] = $dataProvider;
     }
 }
